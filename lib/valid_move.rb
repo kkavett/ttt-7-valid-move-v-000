745797fb558a428a4476c3416369105e54ad416a
Katie Kavett <katie.kavett@flatironschool.com>
2018-09-03 22:41:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if index.between?(0,8) == true && position_taken?(board,index) == true
+  if index.between?(0,8) == true
+    if position_taken?(board,index) == true
       true
+    else
+      false
+    end
   else
     false
   end

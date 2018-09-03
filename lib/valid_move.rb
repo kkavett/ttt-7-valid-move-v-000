ca6ce235cdbb3420516a586a3f4d73e93e07f2c8
Katie Kavett <katie.kavett@flatironschool.com>
2018-09-03 23:54:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,25 +1,20 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-#def position_taken?(board,index)
-#  if board[index] == "X" || "O"
-#    true
-#  elsif board[index] == " " || ""
-#    false
-#  else
-#    true
-#  end
-#end
+def position_taken?(board,index)
+  if board[index] == "X" || "O"
+    true
+  elsif board[index] == " " || ""
+    false
+  else
+    true
+  end
+end
 
 
 # code your #valid_move? method here
 def valid_move?(board,index)
   if index.between?(0,8)
-    if board[index-1] == "X" || "O"
+    if position_taken?(board,index) == true
       true
-    elsif board[index-1] == " "
-      false
-    else
-      false
-    end
   else
     true
   end
